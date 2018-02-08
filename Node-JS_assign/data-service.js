2771913be6e48c4c464801f35f11d1bafed4b559
Gilbertowms <35789072+Gilbertowms@users.noreply.github.com>
2018-02-07 22:03:04 -0400

    Update data-service.js

--- Node-JS_assign/data-service.js
+++ Node-JS_assign/data-service.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 var employees = [];
 var departments = [];
 
-//Initialize function - sta
+//Initialize function - get the information from employees and departments from the json file and store it
+// in variables
 module.exports.initialize = function(){
     return new Promise(function(resolve, reject){
         fs.readFile('./data/employees.json', (err, data) => {
@@ -24,7 +25,7 @@ module.exports.initialize = function(){
     });
 }
 
-
+// Returns a promise - This function get all employees
 module.exports.getAllEmployees = function(){
     return new Promise(function(resolve, reject){
         if(employees.length == 0){
@@ -35,7 +36,8 @@ module.exports.getAllEmployees = function(){
     });
 }
 
-
+// This function gets all employees, but filter the results checking
+//if the isManager property is equal to true
 module.exports.Managers = function(){
     var manager = [];
     return new Promise(function(resolve, reject){
@@ -51,6 +53,7 @@ module.exports.Managers = function(){
 } 
 
 
+// Returns a promise - This function get all Departments
 module.exports.getDepartments = function(){
     return new Promise(function(resolve, reject){
         if(employees.length == 0){
