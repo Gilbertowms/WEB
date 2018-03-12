eadfbd123ef34a03d6f14e190287eaffb22f033b
Gilbertowms <35789072+Gilbertowms@users.noreply.github.com>
2018-03-12 16:11:31 -0400

    Add files via upload

--- Node-JS_assign/data-service.js
+++ Node-JS_assign/data-service.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 var employees = [];
 var departments = [];
 
-//Initialize function - get the information from employees and departments from the json file and store it
-// in variables
+//Initialize function 
 module.exports.initialize = function(){
     return new Promise(function(resolve, reject){
         fs.readFile('./data/employees.json', (err, data) => {
@@ -25,7 +24,7 @@ module.exports.initialize = function(){
     });
 }
 
-// Returns a promise - This function get all employees
+/*This function returns all the information in the employees.json file*/
 module.exports.getAllEmployees = function(){
     return new Promise(function(resolve, reject){
         if(employees.length == 0){
@@ -36,8 +35,8 @@ module.exports.getAllEmployees = function(){
     });
 }
 
-// This function gets all employees, but filter the results checking
-//if the isManager property is equal to true
+/*This function gets the information in the employees.json file, check each one has the isManager = true
+and returns an array that contains all the employees that are managers*/
 module.exports.Managers = function(){
     var manager = [];
     return new Promise(function(resolve, reject){
@@ -45,15 +44,17 @@ module.exports.Managers = function(){
             reject("No results returned");
         }else{
             for(var i = 0; i< employees.length; ++i){
+                if(employees[i].isManager == true)
+                {
                 manager.push(employees[i]);
+                }
             }
             resolve(manager);
         }
     });
 } 
 
-
-// Returns a promise - This function get all Departments
+/*This function returns all the information in the departments.json file*/
 module.exports.getDepartments = function(){
     return new Promise(function(resolve, reject){
         if(employees.length == 0){
@@ -62,4 +63,129 @@ module.exports.getDepartments = function(){
             resolve(departments);
         }
     });
+}
+
+
+/*This function add an employee in the employees.json file*/
+module.exports.addEmployee = function(employeeData){
+    return new Promise((resolve, reject) =>{
+        if(employees.length == 0){
+            reject("No results returned");
+        }
+        else{
+           if(employeeData.isManager == undefined){
+               employeeData.isManager == false;
+            }
+            else{
+                employeeData.isManager == true;
+            }
+            employeeData.employeeNum = employeeData.length + 1;
+            employees.push(employeeData);
+            resolve(employees);
+        }        
+    });
+}
+
+/*This function receives a employee´s status("Full Time" or "Part Time") 
+and return an array that holds all the employees that has that specific status*/
+module.exports.getEmployeesByStatus = (status)=>{
+    var employeesData = [];
+    return new Promise((resolve, reject) =>{
+        if(employees.length == 0){
+            reject("No results returned");
+        }
+        else{
+            for(let i = 0 ; i < employees.length; ++i){
+                if(employees[i].status == status){
+                    employeesData.push(employees[i]);
+                }
+            }
+            resolve(employeesData);
+        }
+    })
+}
+
+
+/*This function receives a department number and returns an array with all the employees that 
+work on this department*/
+module.exports.getEmployeesByDepartment = (department)=>{
+    var departmentData = [];
+    return new Promise((resolve, reject) =>{
+        if(employees.length == 0){
+            reject("No results returned");
+        }
+        else{
+            for(let i = 0 ; i < employees.length; ++i){
+                if(employees[i].department == department){
+                    departmentData.push(employees[i]);
+                }
+            }
+        resolve(departmentData);
+        }
+    })
+}
+
+/* This function receives an employeeNum that belongs to a manager and return all the employees
+that work for the manager that holds this employeeNum */
+module.exports.getEmployeesByManager = (manager)=>{
+    var managerData = [];
+    return new Promise((resolve, reject) =>{
+        if(employees.length == 0){
+            reject("No results returned");
+        }
+        else{
+            for(let i = 0 ; i < employees.length; ++i){
+                if(employees[i].employeeManagerNum == manager){
+                    managerData.push(employees[i]);
+                }
+            }
+        resolve(managerData);
+        }
+    })
+}
+
+/*This function find a specific employee by his employeeNum and return an object with all its data*/
+module.exports.getEmployeeByNum = (num)=>{
+    var empNumData = [];
+    return new Promise((resolve, reject) =>{
+        if(employees.length == 0){
+            reject("No results returned");
+        }
+        else{
+            for(let i = 0 ; i < employees.length; ++i){
+                if(employees[i].employeeNum == num){
+                    empNumData.push(employees[i]);
+                }
+            }
+        resolve(empNumData);
+        }
+    })
+}
+
+/*This function receives an employeeData, through the employeeData.employeeNum search. 
+Once its found, overwrites with the new employeeData passed */
+module.exports.updateEmployee = (employeeData) =>{
+    return new Promise((resolve, reject) =>{
+        if(employees.length == 0){
+            reject("No results returned");
+        }
+        else{
+            for(let i = 0 ; i < employees.length; ++i){
+                if(employees[i].employeeNum == employeeData.employeeNum){
+                    employees[i].firstName = employeeData.firstName;
+                    employees[i].lastName = employeeData.lastName;
+                    employees[i].email = employeeData.email;
+                    employees[i].addressStreet = employeeData.addressStreet;
+                    employees[i].addresCity = employeeData.addresCity;
+                    employees[i].addressState = employeeData.addressState;
+                    employees[i].addressPostal = employeeData.addressPostal;
+                    employees[i].isManager = employeeData.isManager;
+                    employees[i].employeeManagerNum = employeeData.employeeManagerNum;
+                    employees[i].status = employeeData.status;
+                    employees[i].department = employeeData.department;
+                }
+            }
+        resolve();
+        }
+    })
 }
--- Node-JS_assign/server.js
+++ Node-JS_assign/server.js
@@ -1,9 +1,50 @@
+/*****************************************************************************************************
+* WEB322 – Assignment 04 
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part 
+* of this assignment has been copied manually or electronically from any other source 
+* (including 3rd party web sites) or distributed to other students. 
+* 
+* Name: Gilberto Silva Student ID: 139.236.160 Date: 03/12/2018 
+* 
+* Online (Heroku) Link: https://stark-reef-58952.herokuapp.com/
+******************************************************************************************************/
+
+
 //Inserting nodes
-var path = require("path");
-var express = require("express");
-var dataService = require("./data-service.js");
+const exphbs = require("express-handlebars");
+const multer = require("multer");
+const path = require("path");
+const express = require("express");
+const dataService = require("./data-service.js");
+const fs = require('fs');
+const bodyParser = require("body-parser");
 var app = express();
 
+//Setting the server to know how to handle with HTML files that are formated using handlebars
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    helpers: {
+        navLink: function(url, options){
+                return '<li' +
+            ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
+            '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+        },
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+            throw new Error("Handlebars Helper equal needs 2 parameters");
+                if (lvalue != rvalue) {
+                    return options.inverse(this);
+                } else {
+                    return options.fn(this);
+                }
+        }
+    }
+}));
+app.set('view engine', '.hbs');
+
+//Setting the middleware for "urlencoded" form data(normal HTTP Post data)
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Setting the local host port
 var HTTP_PORT = process.env.PORT || 8080;
@@ -13,43 +54,164 @@ function onHttpStart(){
 }
 
 
-
-app.use(express.static('public'));
-
-//Home
-app.get("/", function(req, res) {
-    res.sendFile(path.join( __dirname, "/views/home.html"));
+//Setting the storage name for the uploaded images.
+const storage = multer.diskStorage({
+    destination:"./public/images/uploaded/",
+    filename: function(req, file, cb){
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
 });
 
-//About
-app.get("/about", function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+//This function tells multer to use DiskStorage for naming files instead of the default.
+const upload = multer({storage: storage});
+
+app.use(express.static('./public/'));
+
+//This middleware add the property "activeRoute" to "app.locals" whenever the route changes
+app.use(function(req,res,next){
+    let route = req.baseUrl + req.path;
+    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+    next();
+    });
+
+
+
+//Route of the Home
+app.get("/", (req, res) => {
+    res.render('home');
 });
 
+//route of the About section
+app.get("/about", (req, res) => {
+    res.render('about');
+});
+
+//Routes of the form to add an employee
+app.get("/employees/add", (req, res) =>{
+    res.render('AddEmployees');
+})
+
+app.post("/employees/add",(req, res)=> {
+    dataService.addEmployee(req.body).then(function(data) {
+        res.redirect("/employees");
+    }).catch(function(err) {
+        res.json({ message: err })
+    })  
+})
+
+
+//Route of the form to upload a image
+app.get("/images/add", (req, res) =>{
+     res.render('AddImages');
+})    
+
+ 
+app.post("/images/add", upload.single("imageFile"), (req, res) =>{
+    res.redirect("/images");
+})
+
+
+//Route to show all images uploaded
+app.get("/images", (req, res) => {
+    fs.readdir('./public/images/uploaded', (err, data) => {
+        if (err) {
+            console.log("Unable to read the files!");
+        } else {
+            res.render("images.hbs", {images : data });
+        }
+    })
+});
+
+
+//Route to show Employees (All employees or filtered by a specific parameter)
+app.get("/employees", (req, res) => {
+    const status = req.query.status;
+    const department = req.query.department;
+    const manager = req.query.manager;
+
+
+    //Filtering employees by status "Full Time" or "Part Time"
+    if(status != null){
+        dataService.getEmployeesByStatus(status).then(function(data) {
+            res.render("employees",{employees: data});
+        }).catch(function(err) {
+            res.render({message: "no results"});
+        })                
+    }
+    
+    //Filtering employees by department number from 1 to 7
+    if(department != null){
+        dataService.getEmployeesByDepartment(department).then(function(data) {
+            res.render("employees",{employees: data});
+        }).catch(function(err) {
+            res.render({message: "no results"});
+        })                
+    }
+
+    //Filtering employees by its manager´s employeeNum that has values from 1 to 30    
+    if(manager != null){
+        dataService.getEmployeesByManager(manager).then(function(data) {
+            res.render("employees",{employees: data});
+        }).catch(function(err) {
+            res.render({message: "no results"});
+        })                
+    }
+
+    //If there is no value to filter, it will show the full list of employees
+    if(status == null && department == null && manager == null){
+        dataService.getAllEmployees().then(function(data) {
+            res.render("employees",{employees: data});
+        }).catch(function(err) {
+            res.render({message: "no results"});
+        })
+    }
+});
+
+//Route to select only one specific employee by his employeeNum placed in value
+app.get("/employee/:value", (req, res) => { 
+    const value = req.params.value
+        dataService.getEmployeeByNum(value).then(function(data) {
+            res.render('employee', { employee: data[0] });
+        }).catch(function(err) {
+            res.render('employee', { message:"no results" });
+        })
+});
+
+app.post("/employee/update", (req, res) => { 
+    dataService.updateEmployee(req.body).then(function() {
+        res.redirect("/employees");
+    }).catch(function(err) {
+        res.json({ message: err })
+    })  
+})
+
+/* This route is not being used anymore
 //Managers
-app.get("/managers", function(req, res){
-    dataService.Managers().then(function(data){
+app.get("/managers", (req, res) => {
+    dataService.Managers().then(function(data) {
         res.json(data);
-    }).catch(function(err){
-    res.json({ message: err})})});
-
-//Employees
-app.get("/employees", function(req, res){
-    dataService.getAllEmployees().then(function(data){
-        res.json(data);
-    }).catch(function(err){
-    res.json({ message: err})})});
+    }).catch(function(err) {
+        res.json({
+            message: err
+        })
+    })
+});
+*/
 
 //Departments
-app.get("/departments", function(req, res){
-    dataService.getDepartments().then(function(data){
-        res.json(data);
-    }).catch(function(err)
-    { message: err})});
+app.get("/departments", (req, res)=> {
+    dataService.getDepartments().then(function(data) {
+        res.render("departments", {departments: data});
+    }).catch(function(err) {
+        message: err
+    })
+});
 
+//Page nor found
 app.use(function(req,res){
     res.status(404).send("Page Not Found")
 });
+
 
 // setup http server to listen on HTTP_PORT
 dataService.initialize().then(function(){
@@ -57,3 +219,4 @@ dataService.initialize().then(function(){
 }).catch(function(err){
     console.log("FAILED: " + err);
 });
+
